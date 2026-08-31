29d35cc71b86d10f101a12b3ad4dde1e2ae8f911
Thiago Vailate <thiago.vailate@aluno.fmpsc.edu.br>
2026-08-31 12:37:42 -0300

    Add initial Python program with personal details

---
+++ main.py
@@ -0,0 +1,18 @@
+#Primeiro programa python
+'''print("hello, world!" ' sejam bem-vindos!')
+print("Vamos nessa!")
+
+
+teste de comentários
+bla bla bla bla
+bla bla bla bla
+'''
+
+#nome = input("digite seu nome: ")
+#print("Olá, " + nome)
+
+nome = "Thiago"
+idade = 19
+altura = 1.69
+
+print("Meu nome é", nome, "Tenho", idade, "Anos", "e minha altura é", altura)
